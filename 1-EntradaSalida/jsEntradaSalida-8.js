385561ebe75cc770040862dc28363682681cb633
Sebastian Goldenberg <60668583+SebastianGoldenberg@users.noreply.github.com>
2020-02-05 10:39:30 -0300

    ejercicio 8 correcto

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -3,6 +3,19 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
-{
-	
+{	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=document.getElementById('numeroDividendo').value;
+	primerNumero=parseInt(primerNumero);
+	console.log(primerNumero);
+	segundoNumero=document.getElementById('numeroDivisor').value;
+	console.log(segundoNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	resultado=primerNumero%segundoNumero;
+
+	console.log(resultado);
 }
